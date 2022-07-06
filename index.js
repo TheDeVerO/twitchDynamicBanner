e51c1a96cccc5a5d5f9c090347c53ed9469929ae
TheDeVerO <deverodevero80@gmail.com>
2022-07-06 05:13:38 +0300

    updated things, so that it would actually be usable

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-const button = document.querySelector(`data-test-selector="upload-video-player__banner"`);
-button.click();
+function updateBanner() {
+	const button = document.querySelector(`data-test-selector="upload-video-player__banner"`);
+	button.click();
 
-var upload;
+	var upload;
 
-setTimeout(() => {
-	upload = document.querySelector(`data-a-target="file-picker-input"`);
-	simulateDrop();
-}, 200);
+	setTimeout(() => {
+		upload = document.querySelector(`data-a-target="file-picker-input"`);
+		simulateDrop();
+	}, 200);
 
-upload.ondrop = function (e) {
-	this.className = '';
-	e.preventDefault();
-	readfiles(e.dataTransfer.files);
-};
+	upload.ondrop = function (e) {
+		this.className = '';
+		e.preventDefault();
+		readfiles(e.dataTransfer.files);
+	};
 
-function simulateDrop() {
-	const fileInput = require('./images/japan.png');
-	holder.ondrop({
-		dataTransfer: { files: [fileInput] },
-		preventDefault: function () {},
-	});
+	function simulateDrop() {
+		const fileInput = require('./images/japan.png');
+		holder.ondrop({
+			dataTransfer: { files: [fileInput] },
+			preventDefault: function () {},
+		});
+	}
 }
