548ec3235b1e7daf820fc0f66dfac4b79f35d74a
TheDeVerO <deverodevero80@gmail.com>
2022-07-06 05:07:26 +0300

    index init

---
+++ index.js
@@ -0,0 +1,23 @@
+const button = document.querySelector(`data-test-selector="upload-video-player__banner"`);
+button.click();
+
+var upload;
+
+setTimeout(() => {
+	upload = document.querySelector(`data-a-target="file-picker-input"`);
+	simulateDrop();
+}, 200);
+
+upload.ondrop = function (e) {
+	this.className = '';
+	e.preventDefault();
+	readfiles(e.dataTransfer.files);
+};
+
+function simulateDrop() {
+	const fileInput = require('./images/japan.png');
+	holder.ondrop({
+		dataTransfer: { files: [fileInput] },
+		preventDefault: function () {},
+	});
+}
