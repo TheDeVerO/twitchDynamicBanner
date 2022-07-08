689a69baba0990fd7c4ed197e942df73666fa39b
TheDeVerO <deverodevero80@gmail.com>
2022-07-08 18:28:30 +0300

    first version, works well now

--- index.js
+++ index.js
@@ -3,39 +3,45 @@ const puppeteer = require('puppeteer');
 updateBanner();
 
 async function updateBanner() {
-	const browser = await puppeteer.launch({ headless: false, userDataDir: './user_data' });
+	var browser = await puppeteer.launch({ headless: true, userDataDir: './user_data' });
 	const page = await browser.newPage();
+	page.setDefaultTimeout(0);
 	await page.goto('https://dashboard.twitch.tv/u/the_devero/settings/channel');
 
-	const needLogin = page.evaluate(async () => {
-		document.getElementById('modal-root-header').innerHTML === 'Log in to Twitch' ? true : false;
+	const needLogin = await page.evaluate(async () => {
+		return document.getElementById('modal-root-header')?.innerHTML === 'Log in to Twitch';
 	});
 
 	if (needLogin) {
-		await page.waitForFunction(() => {
-			// Waits for user to input username and password storing it in scoped variable.
-			// It can't be accessed outside of this module.
-			let username = document.getElementById('login-username').value;
-			let password = document.getElementById('login-username').value;
+		browser.close();
+		browser = await puppeteer.launch({ headless: false, userDataDir: './user_data' });
+		page = await browser.newPage();
+		page.setDefaultTimeout(0);
+		await page.goto('https://dashboard.twitch.tv/u/the_devero/settings/channel');
 
-			// As a reprecaution - we don't return login values but instead a boolean if they are empty or not.
-			// Values aren't stored or sent anywhere and are erased as soon as this function ends.
-			return username.length !== 0 && password.length !== 0;
+		await page.waitForFunction(() => {
+			return document.getElementById('modal-root-header')?.innerHTML !== 'Log in to Twitch';
 		});
 	}
 
-	// await page.click('div[class="ScTextWrapper-sc-18v7095-1 eKkyLe"]');
+	await page.waitForSelector('a[title="brand"]');
+
+	await page.click('a[title="brand"]');
+
 	console.log('On needed page.');
 
-	await page.click('button[class="ScCoreButton-sc-1qn4ixc-0 ScCoreButtonSecondary-sc-1qn4ixc-2 ffyxRu kgzEiA"]');
+	await page.waitForSelector('button[data-test-selector="upload-video-player__banner"]');
+
+	await page.click('button[data-test-selector="upload-video-player__banner"]');
 	console.log('Pushed upload button.');
+
+	await page.waitForSelector('input[data-a-target="file-picker-input"]');
 
 	const [fileChooser] = await Promise.all([page.waitForFileChooser(), page.click('input[data-a-target="file-picker-input"]')]);
 	console.log('Got fileUpload');
 
 	await fileChooser.accept(['./images/japan.png']);
 
-	console.log('Accepted.');
-
-	console.log('Click!');
+	await new Promise((resolve) => setTimeout(resolve, 10000));
+	browser.close();
 }
