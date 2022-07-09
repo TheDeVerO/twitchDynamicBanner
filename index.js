59a69932dbac9fe5e3b6928ad1ca782e63b2cea2
TheDeVerO <deverodevero80@gmail.com>
2022-07-09 06:17:27 +0300

    working on configuration function

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
+const readline = require("readline");
 
-updateBanner();
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+var config = require('./config');
+
+main();
 
 async function updateBanner() {
 	// Initial set up.
@@ -74,4 +83,35 @@ async function updateBanner() {
 
 	// Closing the browser to prevent memory leaks.
 	browser.close();
+}
+
+async function createConfig() {
+
+	const newConfig = {};
+
+	rl.question('Twitch Login:', (answer) => {
+		newConfig.login = answer;
+	})
+	// rl.question()
+
+	
+	fs.writeFileSync('./config.json', JSON.stringify(newConfig));
+}
+
+function main () {
+
+	if (!config) {
+		config = await createConfig();
+	}
+	
+	var now = new Date();
+	var millisTill10 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 10, 0, 0, 0) - now;
+	if (millisTill10 < 0) {
+		// millisTill10 += 86400000; // it's after 10am, try 10am tomorrow.
+	}
+	setTimeout(function () {
+		// ...
+	}, millisTill10);
+	
+	updateBanner();
 }
